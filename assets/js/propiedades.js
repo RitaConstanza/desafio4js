75e375949c5a7eb3c61f441623b8edf915c29b71
Rita Valenzuela H <rita.valenzuela.huircan@gmail.com>
2024-12-15 21:34:00 -0300

    first commit

---
+++ assets/js/propiedades.js
@@ -0,0 +1,105 @@
+const propiedades_venta = [
+    {
+      nombre: 'Apartamento de lujo en zona exclusiva',
+      src: 'https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg',
+      descripcion: 'Este apartamento de lujo está ubicado en una exclusiva zona residencial',
+      ubicacion: '123 Luxury Lane, Prestige Suburb, CA 45678',
+      habitaciones: 4,
+      banos: 3,
+      costo: 10000000,
+      smoke: false,
+      pets: false
+    },
+  
+    {
+      nombre: 'Apartamento acogedor en la montaña',
+      src: 'https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg',
+      descripcion: 'Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas',
+      ubicacion: '789 Mountain Road, Summit Peaks, CA 23456',
+      habitaciones: 2,
+      banos: 1,
+      costo: 2000000,
+      smoke: true,
+      pets: true
+    },
+    
+    {
+      nombre: 'Penthouse de lujo con terraza panorámica',
+      src: 'https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg',
+      descripcion: 'Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares',
+      ubicacion: '567 Skyline Avenue, Skyview City, CA 56789',
+      habitaciones: 3,
+      banos: 4,
+      costo: 5500000,
+      smoke: false,
+      pets: true
+    },
+  
+    {
+      nombre: 'Casa familiar a las afueras de la ciudad',
+      src: 'https://d1cfu8v5n1wsm.cloudfront.net/toctoc/fotos/20100908/198482/m_wm_2022030143328801797.jpg',
+      descripcion: 'Casa familiar en Peñaflor, rodeada de naturaleza y tranquilidad.',
+      ubicacion: 'Miraflores 580, Peñaflor',
+      habitaciones: 3,
+      banos: 3,
+      costo: 80000,
+      smoke: false,
+      pets: true
+    }
+    
+  
+    ]
+  
+    const propiedades_alquiler = [
+      
+      {
+        nombre: 'Apartamento en el centro de la ciudad',
+        src: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60',
+        descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+        ubicacion: '123 Main Street, Anytown, CA 91234',
+        habitaciones: 2,
+        banos: 2,
+        costo: 400000,
+        smoke: false,
+        pets: true
+      },
+    
+      {
+        nombre: 'Apartamento luminoso con vista al mar',
+        src: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+        descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
+        ubicacion: '456 Ocean Avenue, Seaside Town, CA 56789',
+        habitaciones: 3,
+        banos: 3,
+        costo: 500000,
+        smoke: true,
+        pets: true
+      },
+    
+      {
+        nombre: 'Condominio moderno en zona residencial',
+        src: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+        descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencialEste hermoso apartamento ofrece una vista impresionante al mar',
+        ubicacion: '123 Main Street, Anytown, CA 91234',
+        habitaciones: 2,
+        banos: 2,
+        costo: 200000,
+        smoke: false,
+        pets: false
+      },
+  
+      {
+        nombre: 'Linda cabaña en Algarrobo, Chile',
+        src: 'https://images.adsttc.com/media/images/570c/3ab7/e58e/ce1a/d000/00a8/newsletter/10.jpg?1460419242',
+        descripcion: 'Cabañas rodeadas de naturaleza en importante comuna de la quinta región',
+        ubicacion: 'José del Valle 400, Algarrobo',
+        habitaciones: 2,
+        banos: 1,
+        costo: 300000,
+        smoke: false,
+        pets: true
+      }
+  
+      ]
+  
+      export { propiedades_venta, propiedades_alquiler }
